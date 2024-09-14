5387d007028579219eaad64ab7159cc3752b8c95
William O'Brien <obrienwilliamobrien@gmail.com>
2024-09-14 14:46:20 +0200

    paborjat python-oversattning

--- payment_method.R
+++ payment_method.R
@@ -1,27 +1,41 @@
 # ladda paket, alla i pkg-vektorn laddas i for-loopen nedan
-pkg <- c("tidyverse", "openxlsx")
+pkg <- c("tidyverse", "openxlsx", "extrafonts")
 lapply(pkg, library, character.only = TRUE)
 
 imp <- openxlsx::read.xlsx("PaymentCaseFromFinland_data.xlsx") %>% 
   as_tibble()
 
+#sanity check: har jag förstått d.tasetet korrekt? en rad per kombination av variabelutfall och customers är hur många som har den kombinationen?
+count(imp, across(-customers)) %>% count(n) # ja det finns bara en rad per kombination av de andra kolumnerna?
+
+grunddata <- imp %>% 
+  mutate(Användare = ifelse(FeatureInvestigated == 1, 'Användare', 'Övriga'))
+
 colors <- c(red = "#ea5545", pink = '#f46a9b', '#ef9b20', '#edbf33', '#ede15b', '#bdcf32', '#87bc45', blue='#27aeef', purple = '#b33dc6')
 
+custom_theme <- function(){
+theme(panel.background = element_blank(),
+  axis.ticks = element_blank(),
+  panel.grid.major.y = element_line(color = "grey"),
+  text = element_text(family = "Helvetica"))
+}
+
+font_import()
+loadfonts(device="win")
+
 by_age_tbl <- imp %>% 
-  count(AgeClass, FeatureInvestigated, customers, name = "Antal") %>%
-  mutate(AntalCustomers = customers*Antal) %>% 
+  group_by(AgeClass, FeatureInvestigated) %>%
+  summarize(Antal = sum(customers, na.rm = TRUE)) %>% 
   group_by(AgeClass) %>% 
-  mutate(Andel = 100*AntalCustomers/sum(AntalCustomers)) %>% 
+  mutate(Andel = 100*Antal/sum(Antal)) %>% 
   ungroup() %>% 
   filter(FeatureInvestigated == 1) %>% 
   mutate(AgeClass = factor(AgeClass, levels = c("Missing", "Group1", "Group2", "Group3", "Group4")))
 
 slide1_by_age <- ggplot(by_age_tbl, aes(AgeClass, Andel)) +
   geom_bar(stat = "identity", fill = colors[["blue"]]) +
-  theme(panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    axis.title = element_blank(),
-    panel.grid.major.y = element_line(color = "grey"))
+  custom_theme()  +
+  theme(axis.title = element_blank())
 
 tbl_2_by_rating <- imp %>% 
   count(Rating, FeatureInvestigated, customers, name = "Antal") %>% 
@@ -64,12 +78,47 @@ slide_3_cube_rating <- ggplot(tbl_3_cube_rating %>% mutate(No = -No), aes(x = cu
   labs(x = "", y = "Cube level")
 
 
-contact <- imp %>% 
-  group_by(FeatureInvestigated) %>% 
-  summarize(`AndelMedEmail` = 100*mean(contact_info_edm, na.rm = TRUE),
-            `AndelMedTelefon` = 100*mean(contact_info_tm, na.rm = TRUE))
-  
+tbl_4_contact <- imp %>% 
+  group_by(HarAnvantMetoden = ifelse(FeatureInvestigated == 1, 'Användare', 'Övriga')) %>% 
+  summarize(`Email` = 100*mean(contact_info_edm, na.rm = TRUE),
+            `Telefon` = 100*mean(contact_info_tm, na.rm = TRUE)) %>% 
+  pivot_longer(c(Email, Telefon), values_to = 'Andel', names_to = 'KontaktInfo')
 
+slide_4_contact <- ggplot(tbl_4_contact, aes(x = KontaktInfo, y = Andel, fill = HarAnvantMetoden)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  theme(panel.background = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        legend.title = element_blank(),
+        panel.grid.major.y = element_line(color = "grey"))
+  
+tbl_5_duration <- grunddata %>% 
+  group_by(DurationClass, Användare) %>% 
+  summarize(Antal = sum(customers, na.rm = TRUE)) %>% 
+  group_by(DurationClass) %>% 
+  mutate(Andel = 100*Antal/sum(Antal)) %>% 
+  ungroup() %>% 
+  mutate(DurationClass = DurationClass %>% 
+           stringr::str_replace_all("to", "till ") %>% 
+           stringr::str_squish() %>% 
+           factor(levels = c("0", "1", "2", "3", "4", "5 till 6", "7 till 9", "10 till 14", "15 till 19", "20 till 29", "30+", "Missing"))) %>% 
+  filter(Användare == 'Användare')
+
+slide_5_duration <- ggplot(tbl_5_duration, aes(x = DurationClass, y = Andel)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.2), fill = colors[["blue"]]) +
+  custom_theme() +
+  theme(axis.title = element_blank())
+
+#TODO börja koda i Python :) 
+
+#summary
+
+# grupp som är äldre med lägre it-vana, förmodligen svårare att byta betalningsmetod
+# 
+
+
+# redovisa 'missing' ifall de känner till bias
+#kolla om jag kan hitta korr cube level och products
 
 
 # users of this method are older, show signs of being less technically proficient, probably difficult to change method
--- payment_method.py
+++ payment_method.py
@@ -1,3 +1,62 @@
 import pandas as pd
 import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from plotnine import ggplot, aes, geom_line, geom_bar, theme, element_blank, element_line, element_text
+
+colors = mcolors.TABLEAU_COLORS
+
+os.chdir('C:/Repos/payment_method_case_if') 
+
+imp = pd.read_excel("PaymentCasefromFinland_data.xlsx")
+
+#sanity check 'grouped values': har jag förstått datan rätt. en rad per kombination av variabelutfall och 'customers' anger hur många med den kombinationen?
+sc_grouped = imp.drop(columns = ['customers']).drop_duplicates()
+
+print(len(sc_grouped) - len(imp)) # finns bara distinkta kombinationer av värden
+
+grunddata = (imp
+             .assign(Användare = lambda x: np.where(x['FeatureInvestigated'] == 1, 'Användare', 'Övriga'),
+                     Rating = lambda x: x['Rating'].str.title())
+             )
+
+def custom_theme(): 
+    return (theme(
+        panel_background=element_blank(),
+        axis_ticks=element_blank(),
+        panel_grid_major_y=element_line(color="grey"),
+        axis_title=element_text(size=14, family='Helvetica')
+    ))
+
+#Slide 1: Vilka åldrar använder metoden?
+by_age_df = (
+    grunddata.groupby(['AgeClass', 'FeatureInvestigated'])
+    .agg(Antal = ('customers', 'sum'))
+    .reset_index()
+    .assign(Andel = lambda x: x.groupby('AgeClass')['Antal'].transform(lambda y:100*y/y.sum()))
+    .query('FeatureInvestigated == 1')
+    .assign(AgeClass = lambda x: pd.Categorical(x['AgeClass'],
+                                                          categories=['Missing', 'Group1', 'Group2', 'Group3', 'Group4'],
+                                                          ordered=True))
+    )
+
+plt_age = (ggplot(by_age_df) +
+           aes(x='AgeClass', y='Andel') +
+           geom_bar(stat = 'identity', fill = colors['tab:blue']) +
+           custom_theme() +
+           theme(axis_title_x = element_blank()))
+
+print(plt_age)
+
+#slide 2 påverkar rating benägenheten?
+by_rating_df = (
+    grunddata.groupby(['Rating', 'FeatureInvestigated'])
+    .agg(Antal = ('customers', 'sum'))
+    .reset_index()
+    .assign(Andel = lambda x: x.groupby('Rating')['Antal'].transform(lambda y:100*y/y.sum()))
+    .query('FeatureInvestigated == 1')
+    .assign(Rating = lambda x:pd.Categorical(x['Rating'],
+                                             categories=['Missing', 'OK', 'Good', 'Star']))
+)
 
